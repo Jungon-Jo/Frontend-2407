0e8193596f37efbbbf7159b85aef3861d688ff33
Jungon Jo <jojungon@gmail.com>
2024-07-23 01:37:30 +0900

    js 반복문

--- 03_JS/ch04_반복문/03_enhancedFor.js
+++ 03_JS/ch04_반복문/03_enhancedFor.js
@@ -1,7 +1,36 @@
-let sumMinute = 0;
-for (let hour = 0; hour <= 23; hour++) {
-  for (let minute = 0; minute <= 59; minute++) {
-    sumMinute++;
-  }
+// Enhanced for 반복문
+
+let summerFruits = ['수박', '참외', '복숭아', '포도'];
+for (let i = 0; i < summerFruits.length; i++) {
+  console.log(i, summerFruits[i]);
 }
-console.log(sumMinute)
+
+for (let i in summerFruits) {
+  console.log(i, summerFruits[i]);
+}
+
+// 배열의 모든 요소에 대해
+for (let fruit of summerFruits) {
+  console.log(fruit);
+}
+
+// 여름 과일들을 하나의 문자열로 만들기
+let favoriteFruits = '';
+for (let fruit of summerFruits)
+  favoriteFruits += fruit + ' ';
+console.log(favoriteFruits);
+
+// 1에서 10까지 곱한 값(10 factorial)
+let product = 1
+for (let i = 1; i <= 10; i++) {
+  product *= i
+}
+console.log(product)
+
+// 1에서 30까지으 3의 배수의 배열 만들기 [3, 6, 9, ..., 30]
+let multiple = [];
+for (let i = 1; i <= 30; i++) {
+  if (i % 3 == 0)
+    multiple.push(i);
+}
+console.log(multiple)
